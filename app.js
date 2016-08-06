2266f113efc3e825294ae3103b1076d3c0213aa0
Vinod Gopalan <kgvinod@gmail.com>
2016-08-06 12:00:25 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var spawn = require('child_process').spawn;
-var _ = require('underscore'); // for some utility goodness
 var util = require('util');
 var exec = require('child_process').exec;
  
@@ -9,13 +8,8 @@ var exec = require('child_process').exec;
 app.get('/', function (req, res) {
     res.send('Hello World!');
 
-    var deploySh = spawn('sh', [ 'deploy.sh' ], {
-        cwd: process.env.HOME,
-        env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
-    });
-
     function puts(error, stdout, stderr) { console.log(stdout) }
-    exec("~/work/node/expressapp/deploy.sh", puts);
+    exec("~/work/node-bash/hello.sh", puts);
 
 });
 
