065ce42a1258344216ddd74dadbff9a92ca22629
Vinod Gopalan <kgvinod@gmail.com>
2016-08-06 17:16:22 -0700

    1st impl complete. Works, but stdout is not formatted

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var app = express();
+var bodyParser     =        require("body-parser");
+
 var spawn = require('child_process').spawn;
 var util = require('util');
-var exec = require('child_process').exec;
- 
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
 
-    function puts(error, stdout, stderr) { console.log(stdout) }
-    exec("~/work/node-bash/hello.sh", puts);
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/run_cmd', function (req, res) {
+    //res.send('Hello World!');
+    //res.send(req.body.cmd);
+
+    var exec = require('child_process').exec;
+    
+    exec(req.body.cmd, function (error, stdout, stderr) {
+      if (error) {
+        console.error('exec error: ${error}');
+        return;
+      }
+      console.log("stdout " + stdout);
+      res.send(stdout);
+      console.log("stderr " + stderr);
+      //res.send(stderr);
+    });    
 
 });
 
